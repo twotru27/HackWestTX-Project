e7d838a7408e2850f78dd61c471b3f7361e4ba13
Yash Kumar <tyushk@gmail.com>
2023-09-16 15:39:24 -0500

    Finalize listing endpoint

--- server/src/market.rs
+++ server/src/market.rs
@@ -1,15 +1,30 @@
 use std::sync::Arc;
 
-use axum::{extract::State, response::IntoResponse};
-use mongodb::{bson::Document, options::FindOptions, Cursor};
+use axum::{extract::State, response::IntoResponse, Json};
+use futures_core::*;
+use mongodb::{
+    bson::{Document, RawDocumentBuf},
+    options::FindOptions,
+    Cursor,
+};
+use serde::{Deserialize, Serialize};
 
-use crate::{AppError, ServerState};
+use crate::{entities::SaleListing, AppError, ServerState};
 
-pub struct ListResponse {}
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ListResponse {
+    listings: Vec<RawDocumentBuf>,
+}
 
-pub async fn list(State(arc): State<Arc<ServerState>>) -> Result<ListResponse, AppError> {
+pub async fn list(State(arc): State<Arc<ServerState>>) -> Result<Json<ListResponse>, AppError> {
     let conn = &arc.db_conn;
-    let collection: Cursor<SaleListing> = conn.collection("listings").find(None, None).await?;
+    let mut collection: Cursor<SaleListing> = conn.collection("listings").find(None, None).await?;
 
-    Ok(ListResponse {})
+    let mut listings = Vec::with_capacity(collection.size_hint().0);
+
+    while let Ok(true) = collection.advance().await {
+        listings.push(collection.current().to_raw_document_buf());
+    }
+
+    Ok(Json(ListResponse { listings }))
 }
