8e2f5b1810bcf2d01ddc8f3a908757ad4d775be0
Yash Kumar <tyushk@gmail.com>
2023-09-16 15:39:36 -0500

    Allow AppError to be used in responses

--- server/src/error.rs
+++ server/src/error.rs
@@ -1,5 +1,17 @@
+use axum::{http::StatusCode, response::IntoResponse};
+
 #[derive(thiserror::Error, Debug)]
 pub enum AppError {
     #[error("database access error: {0}")]
     MongoDBError(#[from] mongodb::error::Error),
+}
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> axum::response::Response {
+        match self {
+            AppError::MongoDBError(e) => {
+                (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()).into_response()
+            }
+        }
+    }
 }
